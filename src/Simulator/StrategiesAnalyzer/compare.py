5b6988d547c7d823bae52ad851477dd2a7860f97
Lazbert <85731679+Lazbert@users.noreply.github.com>
2024-03-15 23:14:26 +0800

    fix: correlation analysis now uses portfolio value instead of monthly ROI

--- src/Simulator/StrategiesAnalyzer/compare.py
+++ src/Simulator/StrategiesAnalyzer/compare.py
@@ -14,26 +14,25 @@ def compare(**params):
     strategies_to_compare = params["strategy_names_to_compare"]
     if len(strategies_to_compare) > 2:
         raise NotImplementedError("Currently only supports comparing 2 strategies")
-    df_strategy_roi = pd.DataFrame(columns=strategies_to_compare)
+    df_strategy_portfolio_value = pd.DataFrame(columns=strategies_to_compare)
 
     # for finding best weights
     metrics_data = list[dict]()
     for strategy in strategies_to_compare:
-        strategy_executed_trades_path = os.path.join(
-            base_dir, strategy, "ExecutedTrades.csv"
+        strategy_daily_budget_path = os.path.join(
+            base_dir, strategy, "DailyBudget.csv"  # "ExecutedTrades.csv"
         )
         strategy_summary_path = os.path.join(
             base_dir, strategy, f"Summary_{strategy}.csv"
         )
-        print(f"Reading {strategy} strategy at: {strategy_executed_trades_path}")
+        print(f"Reading {strategy} strategy at: {strategy_daily_budget_path}")
 
-        df_corr = None
         try:
             df_corr = pd.read_csv(
-                strategy_executed_trades_path,
-                index_col=0,
-                usecols=["symbol", "closing_time", "invested_budget", "gain"],
+                strategy_daily_budget_path,
+                usecols=["Date", "PortfolioValue"],
             )
+            df_strategy_portfolio_value[strategy] = df_corr["PortfolioValue"]
             df_strategy_summary = pd.read_csv(strategy_summary_path, index_col=0).iloc[
                 0
             ]
@@ -47,21 +46,21 @@ def compare(**params):
             )
         except FileNotFoundError as e:
             print(
-                f"File not found: {strategy_executed_trades_path}. Please run simulation for strategy:{strategy} first"
+                f"File not found: {strategy_daily_budget_path}. Please run simulation for strategy:{strategy} first"
             )
             continue
 
-        df_corr["closing_time"] = pd.to_datetime(df_corr["closing_time"], utc=True)
-
-        # since timing of trades in each strategy is different, group them by month of closing time first
-        df_corr = df_corr.groupby(pd.Grouper(key="closing_time", freq="M")).sum()
-        df_corr["monthly_roi"] = (
-            df_corr["gain"] / df_corr["invested_budget"]
-        )  # calculate monthly roi, note gain can be negative (i.e including loss)
-        df_strategy_roi[strategy] = df_corr["monthly_roi"]
+        # """ Monthly ROI approach """
+        # # since timing of trades in each strategy is different, group them by month of closing time first
+        # df_corr["closing_time"] = pd.to_datetime(df_corr["closing_time"], utc=True)
+        # df_corr = df_corr.groupby(pd.Grouper(key="closing_time", freq="M")).sum()
+        # df_corr["monthly_roi"] = (
+        #     df_corr["gain"] / df_corr["invested_budget"]
+        # )  # calculate monthly roi, note gain can be negative (i.e including loss)
+        # df_strategy_roi[strategy] = df_corr["monthly_roi"]
 
     # find correlation between monthly roi of the strategies
-    correlation_matrix = df_strategy_roi.corr()
+    correlation_matrix = df_strategy_portfolio_value.corr()
     print("\n----- Correlation Matrix ----")
     print(correlation_matrix)
     print("----------", end="\n\n")
